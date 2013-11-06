5eb0f2c02c4d4110ba9ce5d1ca7a59c71e67a441
Ownclo - Arcis <mrownclo@gmail.com>
2013-11-06 21:41:23 +0400

    Read segments as plain bytestrings.

--- src/Graphics/JPG.hs
+++ src/Graphics/JPG.hs
@@ -2,31 +2,62 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Graphics.JPG where
 
+import Debug.Trace
+import Numeric
+
 import Control.Applicative
+import Control.Monad
+import Prelude hiding(take)
 
 import Data.Word
 import Data.Char
 import Data.Attoparsec.Char8
+import Data.Attoparsec.Number
 import qualified Data.ByteString.Char8 as BS
 
--- Here will be a JPEG parser.
--- It will be used on the decoder side.
-
 type Frame = BS.ByteString
-data Marker = SOI | EOI
+
+-- supported markers
+data Marker = SOI -- start of input
+            | EOI -- end of input
+            | SOF -- start of frame
     deriving Show
 
-byte :: Char -> Parser Word8
-byte = char8
+markerCode :: Marker -> Char
+markerCode SOI = '\xD8'
+markerCode EOI = '\xD9'
+markerCode SOF = '\xC0'
 
-marker :: Parser Marker
-marker = do
-      byte '\xFF'
-      choice [
-          byte '\xD8' >> return SOI,
-          byte '\xD9' >> return EOI ]
+byte = char
+anyByte = anyChar
+
+marker :: Marker -> Parser Char
+marker m = byte '\xFF' >> byte (markerCode m)
+
+getMarker :: Parser Char
+getMarker = byte '\xFF' >> anyByte
+
+word :: Parser Int
+word = do
+    a <- anyByte
+    b <- anyByte
+    return $ ord(a)*256 + ord(b)
+
+frame :: Parser Frame
+frame = marker SOF >> return "LOL"
+
+segment :: Parser BS.ByteString
+segment = do
+   m <- getMarker
+   l <- word
+   trace ("Marker: " ++ showHex (ord m) " " ++
+          "Length: " ++ show l) $
+       take (l-2)
+
+jpegImage :: Parser [Frame]
+jpegImage = marker SOI >> many segment
 
 main :: IO ()
 main = do
     contents <- BS.readFile "img/sample.jpg"
-    print $ parseOnly marker contents
+    print $ parseOnly jpegImage contents
