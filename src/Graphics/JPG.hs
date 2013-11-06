fe65c28abbf4ed24a15398d19ebd2a9b54e2bce4
Ownclo - Arcis <mrownclo@gmail.com>
2013-11-06 19:55:42 +0400

    Started implementing parser.

--- src/Graphics/JPG.hs
+++ src/Graphics/JPG.hs
@@ -1,7 +1,32 @@
 -- {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Graphics.JPG where
+
+import Control.Applicative
+
+import Data.Word
+import Data.Char
+import Data.Attoparsec.Char8
+import qualified Data.ByteString.Char8 as BS
 
 -- Here will be a JPEG parser.
 -- It will be used on the decoder side.
 
-data Image
+type Frame = BS.ByteString
+data Marker = SOI | EOI
+    deriving Show
+
+byte :: Char -> Parser Word8
+byte = char8
+
+marker :: Parser Marker
+marker = do
+      byte '\xFF'
+      choice [
+          byte '\xD8' >> return SOI,
+          byte '\xD9' >> return EOI ]
+
+main :: IO ()
+main = do
+    contents <- BS.readFile "img/sample.jpg"
+    print $ parseOnly marker contents
