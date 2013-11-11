88676b23877ca8c3683874747424453c4b0e7042
Ownclo - Arcis <mrownclo@gmail.com>
2013-11-11 18:36:33 +0400

    Styling fixes in DCT module

--- src/Graphics/DCT_h.hs
+++ src/Graphics/DCT_h.hs
@@ -4,31 +4,29 @@ import Numeric.Container
 
 -- Normalizing coefficent generator.
 spt :: Int -> Double
-spt x = if (x == 0)
-	then sqrt(0.125)
-	else sqrt(0.25)
+spt 0 = sqrt 0.125
+spt x = sqrt 0.25
 
 -- Perform DCT on a 8x8 matrix.
 dct :: Matrix Double -> Matrix Double
 dct datM = do
-	let dctMat = buildMatrix 8 8 (\(i,j) -> ((spt i) * cos((fromIntegral(j)+0.5)*fromIntegral(i)*pi*0.125)) )
-	let !result = multiply (multiply dctMat datM) (trans dctMat)
-	result
+    let dctMat = buildMatrix 8 8 (\(i,j) -> (spt i * cos((fromIntegral j + 0.5)*fromIntegral i*pi*0.125)) )
+    let !result = multiply (multiply dctMat datM) (trans dctMat)
+    result
 
 -- Perform IDCT on a 8x8 matrix.
 idct :: Matrix Double -> Matrix Double
 idct datM = do
-	let dctMat = buildMatrix 8 8 (\(i,j) -> ((spt j) * cos((fromIntegral(i)+0.5)*fromIntegral(j)*pi*0.125)) )
-	let !result = multiply (multiply dctMat datM) (trans dctMat)
-	result
+    let dctMat = buildMatrix 8 8 (\(i,j) -> (spt j * cos((fromIntegral i + 0.5)*fromIntegral j*pi*0.125)) )
+    let !result = multiply (multiply dctMat datM) (trans dctMat)
+    result
 
 --prepTest :: Int -> [Mddatrix Double]
 --prepTest ret = do
---	let testM = []
+--  let testM = []
 
 -- Main.
 main :: IO()
 main = do 
-
-	let testMat = buildMatrix 8 8 ( \(i,j) -> 2 ^ (fromIntegral(i*j)) )
-	putStrLn . show $ idct (dct testMat)
+    let testMat = buildMatrix 8 8 ( \(i,j) -> fromIntegral(i + j))
+    print $ idct (dct testMat)
